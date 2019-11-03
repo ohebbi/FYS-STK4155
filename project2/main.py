9bb00be79b63c31e81310094c88433d68105507b
Oliver <oliverlh@uio.no>
2019-11-03 10:37:07 +0100

    sklearn nn

--- project2/main.py
+++ project2/main.py
@@ -1,20 +1,24 @@
 from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import  train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-import numpy as np
 from sklearn.linear_model import SGDRegressor
 from random import random, seed
+from pandas import DataFrame
 
 #import functions
+import matplotlib.pyplot as plt
 from functions.neuralnetwork import NeuralNetwork
 from functions.functions import *
-"""
-Simple filter for sklearn
-"""
+#####################
+#Simple filter for sklearn
 from warnings import simplefilter
 # ignore all future warnings
 simplefilter(action='ignore', category=FutureWarning)
-
+#####################
+#vizualization
+import seaborn as sns
+sns.set()
+#####################
 
 cancer = load_breast_cancer()
 #print(len(cancer.target))
@@ -23,6 +27,10 @@ cancer = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(cancer.data,cancer.target,random_state=0)
 
 
+print ("-------------------------------------------------")
+print ("--- Logistic Regression with Gradient Descent ---")
+print ("-------------------------------------------------")
+
 # Scale data
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
@@ -30,7 +38,7 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-theta_linreg = np.linalg.inv(X_train.T.dot(X_train)).dot(X_train.T).dot(y_train)
+#theta_linreg = np.linalg.inv(X_train.T.dot(X_train)).dot(X_train.T).dot(y_train)
 #print("Own inversion")
 #print(theta_linreg)
 
@@ -88,9 +96,7 @@ for i in range(len(y_tilde)):
         I += 1.
 #print (y_tilde)
 accuracy_score = float(I/len(y_tilde))
-print ("-------------------------------------------------")
-print ("--- Logistic Regression with Gradient Descent ---")
-print ("-------------------------------------------------")
+
 print ("Accuracy score on test set:", accuracy_score)
 
 """
@@ -123,6 +129,7 @@ print()
 print ("-------------------------------------------------")
 print ("-------- classification Neural Network ----------")
 print ("-------------------------------------------------")
+"""
 # building our neural network
 n_inputs, n_features = X_train.shape
 n_hidden_neurons = 50
@@ -131,25 +138,81 @@ n_hidden_layers = 2
 
 epochs = 300
 batch_size = 100
-lmbd = 0
-eta = 1e-5
+#lmbd = 0
+#eta = 1e-5
 
 y_train_onehot = to_categorical_numpy(y_train)
 
-dnn = NeuralNetwork(X_data = X_train, Y_data = y_train_onehot, n_hidden_layers=n_hidden_layers,
-                    n_hidden_neurons=n_hidden_neurons, n_categories = n_categories,
-                    epochs = epochs, batch_size=batch_size, eta=eta, lmbd=lmbd,
-                    user_action = 'classification')
-dnn.train()
-test_predict = dnn.predict(X_test)
+eta_vals = np.logspace(-5, 1, 7)
+lmbd_vals = np.logspace(-5, 1, 7)
+
+train_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
+test_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
+
+# store the models for later use
+DNN_numpy = np.zeros((len(eta_vals), len(lmbd_vals)), dtype=object)
+
+import scikitplot.metrics as skplt
+best_data = 0
+# grid search
+for i, eta in enumerate(eta_vals):
+    for j, lmbd in enumerate(lmbd_vals):
+        dnn = NeuralNetwork(X_data = X_train, Y_data = y_train_onehot, n_hidden_layers=n_hidden_layers,
+                            n_hidden_neurons=n_hidden_neurons, n_categories = n_categories,
+                            epochs = epochs, batch_size=batch_size, eta=eta, lmbd=lmbd,
+                            user_action = 'classification')
+        dnn.train()
+
+        DNN_numpy[i][j] = dnn
+
+        train_pred = dnn.predict(X_train)
+        test_pred = dnn.predict(X_test)
+
+        train_accuracy[i][j] = accuracy_score_numpy(y_train, train_pred)
+        test_accuracy[i][j] = accuracy_score_numpy(y_test, test_pred)
+        print("Learning rate  = ", eta)
+        print("Lambda = ", lmbd)
+        print("Accuracy score on test set: ", test_accuracy[i][j])
+        print()
+
+        #cumulative gain plot
+        if test_accuracy[i][j] > best_data:
+            print("hurra")
+
+            best_data = test_accuracy[i][j]
+            test_pred_prob = dnn.predict_probabilities(X_test)
+
+x0,y0 = best_curve(y_test,0)
+x1,y1 = best_curve(y_test,1)
 
 
-print("Accuracy score on test set:", accuracy_score_numpy(y_test, test_predict))
+skplt.plot_cumulative_gain(y_test,test_pred_prob)
+plt.plot(x0,y0,'--',color='royalblue')
+plt.plot(x1,y1,'--',color='darkorange')
+plt.show()
+train_accuracy = DataFrame(train_accuracy, index = eta_vals, columns = lmbd_vals)
 
+fig, ax = plt.subplots(figsize = (6, 6))
+sns.heatmap(train_accuracy, annot=True, ax=ax, cmap="viridis")
+ax.set_title("Training Accuracy for classification")
+ax.set_ylabel("$\eta$")
+ax.set_xlabel("$\lambda$")
+plt.show()
+
+test_accuracy = DataFrame(test_accuracy, index = eta_vals, columns = lmbd_vals)
+
+fig, ax = plt.subplots(figsize = (6, 6))
+sns.heatmap(test_accuracy, annot=True, ax=ax, cmap="viridis")
+ax.set_title("Test Accuracy for classification")
+ax.set_ylabel("$\eta$")
+ax.set_xlabel("$\lambda$")
+plt.show()
+
+"""
 print ("-------------------------------------------------")
 print ("-------------Regression Neural Network-----------")
 print ("-------------------------------------------------")
-
+"""
 x,y,z = generate_data() #FrankeFunction
 
 f = z
@@ -159,42 +222,133 @@ y = sigmoid(y)
 z = sigmoid(z)
 
 x_train, x_test, y_train, y_test, z_train, z_test = train_test_split(x,y,z,random_state=0)
-#z_train = np.reshape(z_train, (20,20))
+
+z_train_inv = inv_sigmoid(z_train)
+z_test_inv = inv_sigmoid(z_test)
+
 
 X_train = find_designmatrix(x_train, y_train, polygrad = 5)
-"""
-scaler.fit(z_train)
-z_train = scaler.transform(z_train)
-z_test = scaler.transform(z_test)
-
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-"""
-
 
 n_inputs, n_features = X_train.shape
 n_categories = 1
+n_hidden_neurons = 50
+n_hidden_layers = 2
 
-dnn = NeuralNetwork(X_data = X_train, Y_data = z_train, n_hidden_layers=n_hidden_layers,
-                    n_hidden_neurons=n_hidden_neurons, n_categories = n_categories,
-                    epochs = epochs, batch_size=batch_size, eta=eta, lmbd=lmbd,
-                    user_action = 'regression')
+epochs = 300
+batch_size = 100
 
+eta_vals = np.logspace(-5, -2, 4)
+lmbd_vals = np.logspace(-8, 0, 9)
 
-dnn.train()
+train_MSE = np.zeros((len(eta_vals), len(lmbd_vals)))
+test_MSE = np.zeros((len(eta_vals), len(lmbd_vals)))
+
+# store the models for later use
+DNN_numpy = np.zeros((len(eta_vals), len(lmbd_vals)), dtype=object)
+
+#Finding designmatrix
 X_test = find_designmatrix(x_test, y_test, polygrad = 5)
 
+# grid search
+for i, eta in enumerate(eta_vals):
+    for j, lmbd in enumerate(lmbd_vals):
+        dnn = NeuralNetwork(X_data = X_train, Y_data = z_train, n_hidden_layers=n_hidden_layers,
+                            n_hidden_neurons=n_hidden_neurons, n_categories = n_categories,
+                            epochs = epochs, batch_size=batch_size, eta=eta, lmbd=lmbd,
+                            user_action = 'regression')
+        dnn.train()
+
+        DNN_numpy[i][j] = dnn
+
+        train_pred = dnn.predict_probabilities(X_train)
+        test_pred = dnn.predict_probabilities(X_test)
+
+        train_pred = inv_sigmoid(train_pred)
+        test_pred = inv_sigmoid(test_pred)
+
+
+        train_MSE[i][j] = MSE(z_train_inv, train_pred)[0]
+        test_MSE[i][j] = MSE(z_test_inv, test_pred)[0]
+
+        print("Learning rate  = ", eta)
+        print("Lambda = ", lmbd)
+        print("Mean square error on test set:", test_MSE[i][j])
+        print()
+
+
+
+
+fig, ax = plt.subplots(figsize = (6, 6))
+sns.heatmap(train_MSE, annot=True, ax=ax, cmap="viridis")
+ax.set_title("MSE for regression")
+ax.set_ylabel("$\eta$")
+ax.set_xlabel("$\lambda$")
+plt.show()
+
+fig, ax = plt.subplots(figsize = (6, 6))
+sns.heatmap(test_MSE, annot=True, ax=ax, cmap="viridis")
+ax.set_title("MSE for regression")
+ax.set_ylabel("$\eta$")
+ax.set_xlabel("$\lambda$")
+plt.show()
+
 #X_test = scaler.transform(X_test)
-
-test_predict = dnn.predict_probabilities(X_test)
-
-test_predict = inv_sigmoid(test_predict)
-z_test = inv_sigmoid(z_test)
-
-print(MSE(z_test,test_predict))
 
 
 g = inv_sigmoid(z)
 print(np.mean(f-g))
 
 #def test_scaler():
+"""
+print ("-------------------------------------------------")
+print ("----Sklearn Regression Neural Network-----------")
+print ("-------------------------------------------------")
+
+from sklearn.neural_network import MLPClassifier
+# store models for later use
+DNN_scikit = np.zeros((len(eta_vals), len(lmbd_vals)), dtype=object)
+
+for i, eta in enumerate(eta_vals):
+    for j, lmbd in enumerate(lmbd_vals):
+        dnn = MLPClassifier(hidden_layer_sizes=(n_hidden_neurons), activation='logistic',
+                            alpha=lmbd, learning_rate_init=eta, max_iter=epochs,
+                            )
+        dnn.fit(X_train, z_train)
+
+        DNN_scikit[i][j] = dnn
+
+        print("Learning rate  = ", eta)
+        print("Lambda = ", lmbd)
+        print("Accuracy score on test set: ", dnn.score(X_test, z_test))
+        print()
+
+train_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
+test_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
+
+for i in range(len(eta_vals)):
+    for j in range(len(lmbd_vals)):
+        dnn = DNN_scikit[i][j]
+
+        train_pred = dnn.predict(X_train)
+        test_pred = dnn.predict(X_test)
+
+        train_pred = inv_sigmoid(train_pred)
+        test_pred = inv_sigmoid(test_pred)
+
+        train_accuracy[i][j] = accuracy_score(z_train, train_pred)
+        test_accuracy[i][j] = accuracy_score(z_test, test_pred)
+
+
+fig, ax = plt.subplots(figsize = (10, 10))
+sns.heatmap(train_accuracy, annot=True, ax=ax, cmap="viridis")
+ax.set_title("Training Accuracy")
+ax.set_ylabel("$\eta$")
+ax.set_xlabel("$\lambda$")
+plt.show()
+
+fig, ax = plt.subplots(figsize = (10, 10))
+sns.heatmap(test_accuracy, annot=True, ax=ax, cmap="viridis")
+ax.set_title("Test Accuracy")
+ax.set_ylabel("$\eta$")
+ax.set_xlabel("$\lambda$")
+plt.show()
