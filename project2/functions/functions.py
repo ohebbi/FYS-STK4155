b4387c1629789c5855a1240c246343b12e7c9cfd
Oliver <oliverlh@uio.no>
2019-11-05 13:53:15 +0100

    added jupyter notebook

--- project2/functions/functions.py
+++ project2/functions/functions.py
@@ -1,5 +1,8 @@
-
+import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 
 def FrankeFunction(x,y):
@@ -32,24 +35,59 @@ def generate_data(number_points = 20, plott = True):
     x, y = np.meshgrid(x_data,y_data)
 
     z = FrankeFunction(x, y)
-    """
+
     if plott == True:
         plotter(x,y,z)
-        plt.savefig('plots/Franke/frankefunction.pdf')
-    """
+        #plt.savefig('plots/Franke/frankefunction.pdf')
+
     #flatten the matrix out
     x = np.ravel(x)
     y = np.ravel(y)
     z = np.ravel(z)
 
-    print ("x ranges from", 0, "to", 1, "with a total amount of", number_points, "points.")
-    print ("y ranges from", 0, "to", 1, "with a total amount of", number_points, "points.")
+    print ("x ranges from", 0, "to", 1, "with a total amount of", number_points**2, "points.")
+    print ("y ranges from", 0, "to", 1, "with a total amount of", number_points**2, "points.")
 
 
     eps = np.random.normal(0,1,len(z))
     z += 0.01*eps
 
     return x, y, z
+
+def R2(z_data, z_model):
+    """
+    Function:
+    Finds the R2-level for a given model and approximation.
+    Input:
+    Takes an array z_data and z_model.
+    Output:
+    Returns a scalar.
+    """
+    return (1 - np.sum( (z_data - z_model)**2 ) / np.sum((z_data - np.mean(z_data))**2))
+    
+def plotter(x,y,z):
+    """
+    Function:
+    Generates a three dimensional plot.
+    Input:
+    Takes an array x, y and z.
+    Output:
+    Gives a plot.
+    """
+    fig = plt.figure();
+    ax = fig.gca(projection='3d');
+    # Plot the surface.
+    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False);
+                       # Customize the z axis.
+    ax.set_zlim(-0.10, 1.40);
+    ax.zaxis.set_major_locator(LinearLocator(10));
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'));
+    for angle in range(0,150):
+        ax.view_init(40,angle)
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5);
+
 def MSE(z_data,z_model):
     """
     Function:
@@ -138,5 +176,15 @@ def to_categorical_numpy(integer_vector):
     onehot_vector[range(n_inputs), integer_vector] = 1
 
     return onehot_vector
+
 def accuracy_score_numpy(Y_test, Y_pred):
     return np.sum(Y_test == Y_pred) / len(Y_test)
+
+def best_curve(y,summ):
+	defaults = sum(y == summ)
+	total = len(y)
+	x = np.linspace(0, 1, total)
+	y1 = np.linspace(0, 1, defaults)
+	y2 = np.ones(total-defaults)
+	y3 = np.concatenate([y1,y2])
+	return x, y3
